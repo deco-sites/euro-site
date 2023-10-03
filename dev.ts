e17e998b8ef3f5271cd14c6ee0bd8b58539c816b
Paulo Gomes <paulo.gomes@eurocolchoes.com.br>
2023-10-03 10:39:50 -0300

    atualização do site para apps

--- dev.ts
+++ dev.ts
@@ -1,16 +1,5 @@
-#!/usr/bin/env -S deno run -A --watch
-import dev from "$live/dev.ts";
-import liveManifest from "$live/live.gen.ts";
-import liveStdManifest from "deco-sites/std/live.gen.ts";
-import tailwind from "deco-sites/std/tailwindv3.ts";
+// #!/usr/bin/env -S deno run -A --watch=static/
+import dev from "$fresh/dev.ts";
+import "https://deno.land/x/dotenv@v3.2.2/load.ts";
 
-// Start tailwind background process generation
-tailwind();
-
-// Generate manifest and boot server
-await dev(import.meta.url, "./main.ts", {
-  imports: {
-    "$live": liveManifest,
-    "deco-sites/std": liveStdManifest,
-  },
-});
+await dev(import.meta.url, "./main.ts");
