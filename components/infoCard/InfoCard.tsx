7e1e96a8cdff0b6c958d4373d6ad0d6c5c884f67
Paulo Gomes <paulo.gomes@eurocolchoes.com.br>
2023-10-03 17:29:20 -0300

    Alinhamento de subtitulo InfoCard

--- components/infoCard/InfoCard.tsx
+++ components/infoCard/InfoCard.tsx
@@ -35,7 +35,7 @@ export default function InfoCard(
 
   return (
     <section class={`${textAlignment} pt-8`}>
-      <h6 class="text-emphasis font-bold text-xs uppercase">{caption}</h6>
+      <h6 class="text-center text-emphasis font-bold text-xs uppercase">{caption}</h6>
       <h3 class="text-center text-secondary font-normal text-2xl mb-5">{title}</h3>
       {links?.length
         ? (
