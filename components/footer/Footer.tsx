286c3ad0f1b09e479b1c67d07dcf3d762e379391
deveurocolchoes <136468983+deveurocolchoes@users.noreply.github.com>
2023-11-01 18:03:01 -0300

    Update Footer.tsx

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -66,6 +66,11 @@ export interface Props {
    * @title Phone number
    */
   phone: string;
+
+  /**
+   * @title Cel number
+   */
+  celular: string;
   /**
    * @title E-mail
    */
@@ -97,6 +102,7 @@ function Footer(
     openingHours,
     email,
     phone,
+    celular,
     payments,
     securities,
     poweredby,
@@ -115,6 +121,7 @@ function Footer(
               <Icon id="Phone" width={20} height={20} class="text-emphasis" />
               <span class="text-emphasis text-xs font-bold text-left">
                 {phone}
+                {celular]
               </span>
             </div>
             <div class="flex items-center gap-2 mt-3">
